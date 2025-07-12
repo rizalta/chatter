e4aabe2be2935717f7b2d2542fc25e95fa0ed5a3
rizalta <rizalta00@gmail.com>
2025-07-12 20:12:17 +0530

    prototype chatroom

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let messages = $state<string[]>([]);
+	let newMessage = $state('');
+	let socket: WebSocket;
+
+	onMount(() => {
+		socket = new WebSocket('ws://localhost:8080/ws');
+
+		socket.onmessage = (event) => {
+			messages = [...messages, event.data];
+		};
+
+		socket.onclose = () => {
+			messages = [...messages, 'Connection closed'];
+		};
+	});
+
+	function send() {
+		if (newMessage && socket.readyState == WebSocket.OPEN) {
+			socket.send(newMessage);
+			newMessage = '';
+		}
+	}
+</script>
+
+<h1>Chatter</h1>
+<ul>
+	{#each messages as message, i (i)}
+		<li>{message}</li>
+	{/each}
+</ul>
+
+<form onsubmit={send}>
+	<input type="text" bind:value={newMessage} />
+	<button type="submit">Send</button>
+</form>
