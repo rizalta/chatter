c047443b36fcf7751b6d4b81d94daf7e1a09d2a8
rizalta <rizalta00@gmail.com>
2025-08-04 23:31:26 +0530

    modified chat bubble component

--- app/src/lib/components/ui/chat_bubble/chat_bubble.svelte
+++ app/src/lib/components/ui/chat_bubble/chat_bubble.svelte
@@ -37,22 +37,26 @@
 	}
 </script>
 
-<div class={cn('flex items-end gap-2', isUser && 'flex-row-reverse')} {...rest}>
-	<Avatar class="h-8 w-8">
-		<AvatarFallback>{getInitials(message.fromName)}</AvatarFallback>
+<div class={cn('flex items-center gap-2', isUser && 'flex-row-reverse')} {...rest}>
+	<Avatar class="h-9 w-9">
+		<AvatarFallback>
+			{getInitials(message.fromName)}
+		</AvatarFallback>
 	</Avatar>
-	<div class="flex flex-col">
+	<div class="flex flex-col gap-1">
+		{#if !isUser}
+			<p class="text-foreground/80 mb-1 text-xs font-semibold">{message.fromName}</p>
+		{/if}
 		<div
 			class={cn(
 				'group relative max-w-sm rounded-lg px-3 py-2 shadow-sm',
 				isUser ? 'bg-primary text-primary-foreground rounded-br-none' : 'bg-muted rounded-bl-none'
 			)}
 		>
-			{#if !isUser}
-				<p class="text-foreground/80 mb-1 text-xs font-semibold">{message.fromName}</p>
-			{/if}
 			<p class="text-sm whitespace-pre-wrap">{message.content}</p>
 		</div>
-		<span class="text-muted-foreground text-sm">{formatTime(message.timestamp)}</span>
+		<span class={cn('text-muted-foreground text-sm', isUser ? 'text-end' : 'text-start')}
+			>{formatTime(message.timestamp)}</span
+		>
 	</div>
 </div>
